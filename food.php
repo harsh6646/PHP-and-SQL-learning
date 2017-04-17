05b7bdc669d4483df4b66da73781c94d7cc931cf
Harsh Patel <harsh98@hotmail.com>
2017-04-16 21:57:38 -0400

    prep version 2 files

---
+++ food.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shayona Foods</title>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+</head>
+<body>
+
+	<script type="text/javascript" src="/scripts/ajax_script.js"></script>
+	<script type="text/javascript" src="/scripts/validate.js"></script>
+</body>
+</html>
